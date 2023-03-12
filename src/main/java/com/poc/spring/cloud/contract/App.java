a044d631c4e58b41186f5ce1133569768bd7ac62
Siddalinga <itssiddu93@gmail.com>
2023-03-12 21:44:15 +0530

    initial commit

---
+++ src/main/java/com/poc/spring/cloud/contract/App.java
@@ -0,0 +1,13 @@
+package com.poc.spring.cloud.contract;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
